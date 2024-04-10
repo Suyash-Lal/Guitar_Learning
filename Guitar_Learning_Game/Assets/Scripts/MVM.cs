e3effb5a9bc6741a728cd0cd2f625270cc5d77d5
Suyash Lal <suyash.a.lal@gmail.com>
2024-04-10 13:18:22 +0530

    audio clips added

--- Guitar_Learning_Game/Assets/Scripts/MVM.cs
+++ Guitar_Learning_Game/Assets/Scripts/MVM.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MVM : MonoBehaviour
 {
+    public float movespeed = 5f;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
