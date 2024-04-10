6ab5b2d44f5f31d60a9a69e999df9e01751d7098
Suyash Lal <suyash.a.lal@gmail.com>
2024-04-11 05:00:19 +0530

    audio handling scripts added

---
+++ Guitar_Learning_Game/Assets/Scripts/YIN_Suyash.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import fftconvolve
+import os
+from os import sep
+from audio_processing import *
+import time
+
+def differenceFunction_original(x, N, tau_max):
+    """
+    Compute difference function of data x. This corresponds to equation (6) in [1]
+
+    Original algorithm.
+
+    :param x: audio data
+    :param N: length of data
+    :param tau_max: integration window size
+    :return: difference function
+    :rtype: list
+    """
+    df = [0] * tau_max
+    for tau in range(1, tau_max):
+         for j in range(0, N - tau_max):
+             tmp = int(x[j] - x[j + tau])
+             df[tau] += tmp * tmp
+    return df
+
+def differenceFunction_scipy(x, N, tau_max):
+    """
+    Compute difference function of data x. This corresponds to equation (6) in [1]
+
+    Faster implementation of the difference function.
+    The required calculation can be easily evaluated by Autocorrelation function or similarly by convolution.
+    Wiener–Khinchin theorem allows computing the autocorrelation with two Fast Fourier transforms (FFT), with time complexity O(n log n).
+    This function use an accellerated convolution function fftconvolve from Scipy package.
+
+    :param x: audio data
+    :param N: length of data
+    :param tau_max: integration window size
+    :return: difference function
+    :rtype: list
+    """
+    x = np.array(x, np.float64)
+    w = x.size
+    x_cumsum = np.concatenate((np.array([0]), (x * x).cumsum()))
+    conv = fftconvolve(x, x[::-1])
+    tmp = x_cumsum[w:0:-1] + x_cumsum[w] - x_cumsum[:w] - 2 * conv[w - 1:]
+    return tmp[:tau_max + 1]
+
+
+def differenceFunction(x, N, tau_max):
+    """
+    Compute difference function of data x. This corresponds to equation (6) in [1]
+
+    Fastest implementation. Use the same approach than differenceFunction_scipy.
+    This solution is implemented directly with Numpy fft.
+
+
+    :param x: audio data
+    :param N: length of data
+    :param tau_max: integration window size
+    :return: difference function
+    :rtype: list
+    """
+
+    x = np.array(x, np.float64)
+    w = x.size
+    tau_max = min(tau_max, w)
+    x_cumsum = np.concatenate((np.array([0.]), (x * x).cumsum()))
+    size = w + tau_max
+    p2 = (size // 32).bit_length()
+    nice_numbers = (16, 18, 20, 24, 25, 27, 30, 32)
+    size_pad = min(x * 2 ** p2 for x in nice_numbers if x * 2 ** p2 >= size)
+    fc = np.fft.rfft(x, size_pad)
+    conv = np.fft.irfft(fc * fc.conjugate())[:tau_max]
+    return x_cumsum[w:w - tau_max:-1] + x_cumsum[w] - x_cumsum[:tau_max] - 2 * conv
+
+
+
+def cumulativeMeanNormalizedDifferenceFunction(df, N):
+    """
+    Compute cumulative mean normalized difference function (CMND).
+
+    This corresponds to equation (8) in [1]
+
+    :param df: Difference function
+    :param N: length of data
+    :return: cumulative mean normalized difference function
+    :rtype: list
+    """
+
+    cmndf = df[1:] * range(1, N) / np.cumsum(df[1:]).astype(float) #scipy method
+    return np.insert(cmndf, 0, 1)
+
+
+
+def getPitch(cmdf, tau_min, tau_max, harmo_th=0.1):
+    """
+    Return fundamental period of a frame based on CMND function.
+
+    :param cmdf: Cumulative Mean Normalized Difference function
+    :param tau_min: minimum period for speech
+    :param tau_max: maximum period for speech
+    :param harmo_th: harmonicity threshold to determine if it is necessary to compute pitch frequency
+    :return: fundamental period if there is values under threshold, 0 otherwise
+    :rtype: float
+    """
+    tau = tau_min
+    while tau < tau_max:
+        if cmdf[tau] < harmo_th:
+            while tau + 1 < tau_max and cmdf[tau + 1] < cmdf[tau]:
+                tau += 1
+            return tau
+        tau += 1
+
+    return 0    # if unvoiced
+
+
+
+def compute_yin(sig, sr, dataFileName=None, w_len=512, w_step=256, f0_min=100, f0_max=700, harmo_thresh=0.1):
+    """
+
+    Compute the Yin Algorithm. Return fundamental frequency and harmonic rate.
+
+    :param sig: Audio signal (list of float)
+    :param sr: sampling rate (int)
+    :param w_len: size of the analysis window (samples)
+    :param w_step: size of the lag between two consecutives windows (samples)
+    :param f0_min: Minimum fundamental frequency that can be detected (hertz)
+    :param f0_max: Maximum fundamental frequency that can be detected (hertz)
+    :param harmo_tresh: Threshold of detection. The yalgorithmù return the first minimum of the CMND fubction below this treshold.
+
+    :returns:
+
+        * pitches: list of fundamental frequencies,
+        * harmonic_rates: list of harmonic rate values for each fundamental frequency value (= confidence value)
+        * argmins: minimums of the Cumulative Mean Normalized DifferenceFunction
+        * times: list of time of each estimation
+    :rtype: tuple
+    """
+
+    print('Yin: compute yin algorithm')
+    tau_min = int(sr / f0_max)
+    tau_max = int(sr / f0_min)
+
+    timeScale = range(0, len(sig) - w_len, w_step)  # time values for each analysis window
+    times = [t/float(sr) for t in timeScale]
+    frames = [sig[t:t + w_len] for t in timeScale]
+
+    pitches = [0.0] * len(timeScale)
+    harmonic_rates = [0.0] * len(timeScale)
+    argmins = [0.0] * len(timeScale)
+
+    for i, frame in enumerate(frames):
+
+        #Compute YIN
+        df = differenceFunction(frame, w_len, tau_max)
+        cmdf = cumulativeMeanNormalizedDifferenceFunction(df, tau_max)
+        p = getPitch(cmdf, tau_min, tau_max, harmo_thresh)
+
+        #Get results
+        if np.argmin(cmdf)>tau_min:
+            argmins[i] = float(sr / np.argmin(cmdf))
+        if p != 0: # A pitch was found
+            pitches[i] = float(sr / p)
+            harmonic_rates[i] = cmdf[p]
+        else: # No pitch, but we compute a value of the harmonic rate
+            harmonic_rates[i] = min(cmdf)
+
+
+    if dataFileName is not None:
+        np.savez(dataFileName, times=times, sr=sr, w_len=w_len, w_step=w_step, f0_min=f0_min, f0_max=f0_max, harmo_thresh=harmo_thresh, pitches=pitches, harmonic_rates=harmonic_rates, argmins=argmins)
+        print('\t- Data file written in: ' + dataFileName)
+
+    return pitches, harmonic_rates, argmins, times
+
+def calculate_average_frequency(pitches):
+    """Calculate the average frequency from detected pitches, excluding zeros (unvoiced segments)."""
+    non_zero_pitches = [pitch for pitch in pitches if pitch > 0]
+    if non_zero_pitches:
+        return sum(non_zero_pitches) / len(non_zero_pitches)
+    else:
+        return 0
+
+def find_closest_guitar_note(frequency):
+    # Define the notes and their frequencies in one octave
+    notes = ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E (next octave)']
+    frequencies = [82, 87, 93, 98, 104, 110, 117, 124, 131, 139, 147, 156, 165]
+
+    # This method avoids an infinite loop by ensuring the loop condition will eventually fail
+    octave = 1
+    while True:
+        octave *= 2
+        new_frequencies = [f * octave for f in frequencies[:12]]  # Skip the last note to avoid duplication
+        if new_frequencies[0] > 250:
+            break
+        frequencies.extend(new_frequencies)
+        notes.extend(notes[:12])
+
+    # Find the closest frequency
+    closest_freq = min(frequencies, key=lambda x: abs(x - frequency))
+    note_index = frequencies.index(closest_freq)
+    return notes[note_index % 12], closest_freq
+
+
+
+
+
+def main(audioFileName="OpenE.wav", w_len=1024, w_step=256, f0_min=70, f0_max=700, harmo_thresh=0.85, audioDir="./", dataFileName=None, verbose=4):
+    """
+    Run the computation of the Yin algorithm on a example file.
+
+    Write the results (pitches, harmonic rates, parameters ) in a numpy file.
+
+    :param audioFileName: name of the audio file
+    :type audioFileName: str
+    :param w_len: length of the window
+    :type wLen: int
+    :param wStep: length of the "hop" size
+    :type wStep: int
+    :param f0_min: minimum f0 in Hertz
+    :type f0_min: float
+    :param f0_max: maximum f0 in Hertz
+    :type f0_max: float
+    :param harmo_thresh: harmonic threshold
+    :type harmo_thresh: float
+    :param audioDir: path of the directory containing the audio file
+    :type audioDir: str
+    :param dataFileName: file name to output results
+    :type dataFileName: str
+    :param verbose: Outputs on the console : 0-> nothing, 1-> warning, 2 -> info, 3-> debug(all info), 4 -> plot + all info
+    :type verbose: int
+    """
+
+    if audioDir is not None:
+        audioFilePath = audioDir + sep + audioFileName
+    else:
+        audioFilePath = audioFileName
+
+    audioFilePath = os.path.join(audioDir, audioFileName)
+
+    sr, sig = audio_read(audioFilePath, formatsox=False)
+
+    start = time.time()
+    pitches, harmonic_rates, argmins, times = compute_yin(sig, sr, dataFileName, w_len, w_step, f0_min, f0_max, harmo_thresh)
+    end = time.time()
+    print("Yin computed in: ", end - start)
+
+    duration = len(sig)/float(sr)
+
+    
+    # After computing the yin pitches
+    average_frequency = calculate_average_frequency(pitches)
+    print("Average Frequency:", average_frequency)
+    average_frequency = calculate_average_frequency(pitches)
+    closest_note, closest_frequency = find_closest_guitar_note(average_frequency)
+    print(f"The closest guitar note to the average frequency {average_frequency} Hz is {closest_note} ({closest_frequency} Hz).")
+    return closest_note
+
+
+'''
+    GRAPH REPRESENTATION:
+
+    if verbose >3:
+        ax4 = plt.subplot(4,1,2)
+        ax4.plot([float(x) * duration / len(argmins) for x in range(0, len(argmins))], argmins)
+        ax4.set_title('Index of minimums of CMND')
+        ax4.set_ylabel('Frequency (Hz)')
+        ax4.set_xlabel('Time (seconds)')
+        plt.show()
+'''
+
+if __name__ == '__main__':
+    main()
---
+++ Guitar_Learning_Game/Assets/Scripts/audio_processing.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+_author__ = "Patrice Guyot"
+__email__ = "patrice.guyot@irit.fr"
+__version__ = "1.1.0"
+
+"""
+***********************************************************************
+Name            : audio_processing.py
+Description     : The module implements a function to read audio file.
+Author          : Patrice Guyot.
+***********************************************************************
+"""
+
+
+
+from scipy.io.wavfile import read as wavread
+import logging
+from os import remove, sep
+import subprocess
+from pydub import AudioSegment
+
+AudioSegment.ffmpeg = 'C:\FFmpeg'
+def audio_read(audioFilePath, formatsox=False):
+    """
+
+    Read an audio file (from scipy.io.wavfile)
+
+    A conversation of the aufio file can be processed using SOX (by default set at False).
+
+    :param audioFilePath: audio file name (with eventually the full path)
+    :type audioFilePath: str
+    :param formatsox: if set at True, us SOX to convert audio file in "wav file, 1 channel, 16 kHz, 16bits". Default: False
+    :type audioFilePath: str
+
+    :returns:
+        * sr: sampling rate of the signal
+        * sig: list of values of the signal
+    :rtype: tuple
+
+    """
+    if audioFilePath.lower().endswith('.mp3') and formatsox:
+        # Convert MP3 to WAV using pydub
+        audio = AudioSegment.from_mp3(audioFilePath)
+        audioFilePath = audioFilePath.replace('.mp3', '.wav')
+        audio.export(audioFilePath, format="wav")
+
+    logging.info('Reading of the audio file : ' + audioFilePath)
+    if formatsox:
+        tmpFile = "tmp.wav"
+        logging.info('\t- Conversion en wav 16k with SOX.')
+        cmd = "sox " + audioFilePath + " -c 1 -r 16k -b 16 -G " + tmpFile + " rate -m"
+        subprocess.check_call(cmd)
+        [sr, sig] = wavread(tmpFile)
+        remove(tmpFile)
+    else:
+        [sr, sig] = wavread(audioFilePath)
+
+    return sr, sig
